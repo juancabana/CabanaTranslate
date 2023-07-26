55ff69130c0382050c48f73df2b6d3d78a5d059b
juancabana <juand.cabanat@unac.edu.co>
2023-07-26 09:39:49 -0500

    inicializating MterialUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+import { Fragment } from 'react'
+import { Container, Box, CssBaseline } from '@mui/material'
 
-function App() { 
-  const [count, setCount] = useState(0)
+function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Fragment>
+      <CssBaseline/>
+      <Container maxWidth='lg'>
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+          
+        </Box>
+      </Container>
+    </Fragment>
   )
 }
 
